9b9617fa5c941d9a851050f390116714949d96aa
Jacob Miles <jacob.e.miles@pm.me>
2023-12-12 18:37:13 -0600

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Hyprland Virtual Desktops";
+
+  inputs.hyprland.url = "github:hyprwm/Hyprland";
+
+  outputs = {
+    self,
+    hyprland,
+  }: let
+    inherit (hyprland.inputs) nixpkgs;
+    withPkgsFor = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
+  in {
+    packages = withPkgsFor (system: pkgs: {
+      hyprland-virtual-desktops = pkgs.callPackage ./hyprland-virtual-desktop {
+        inherit (hyprland.packages.${system}) hyprland;
+	stdenv = pkgs.gcc13Stdenv;
+      };
+    };
+
+    devShells = withPkgsFor (system: pkgs: {
+      default = pkgs.mkShell.override {stdenv = pkgs.gcc13Stdenv;}{
+        name = "hyprland-vertial-desktops";
+	buildInputs = [hyprland.packages.${system}.hyprland];
+	inputsFrom = [hyprland.packages.${system}.hyprland];
+      };
+    };
+  };
+}
